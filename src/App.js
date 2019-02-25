f8ec7e81220227eafad555e17034327fd2cedfc3
pskali1998 <pompolompo2@gmail.com>
2019-02-25 22:31:00 +0530

    Just adding changes for test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Route } from "react-router-dom";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
-import Dashboard from "./components/admin/dashboard";
-import ProductDetails from "./components/productDetails";
-import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
